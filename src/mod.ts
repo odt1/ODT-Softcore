ca7bcc93e80c594357cb692868fe0876a1e77774
odt1 <33424002+odt1@users.noreply.github.com>
2024-04-02 22:51:30 +0300

    Fixes

--- src/mod.ts
+++ src/mod.ts
@@ -704,6 +704,181 @@ class Mod implements IPostDBLoadMod {
 			}
 		}
 
+		if (config.HideoutOptions.enabled) {
+			if (config.HideoutOptions.StashOptions.enabled) {
+				// Fix for ADHD.
+
+				// stash area id 5d484fc0654e76006657e0ab
+				// "_id": "566abbc34bdc2d92178b4576",
+				// "_name": "Standard stash 10x28",
+				// "_id": "5811ce572459770cba1a34ea",
+				// "_name": "Left Behind stash 10x38",
+				// "_id": "5811ce662459770f6f490f32",
+				// "_name": "Prepare for escape stash 10x48",
+				// "_id": "5811ce772459770e9e5f9532",
+				// "_name": "Edge of darkness stash 10x68",
+
+				if (config.HideoutOptions.StashOptions.BiggerStash.enabled) {
+					try {
+						items["566abbc34bdc2d92178b4576"]._props.Grids[0]._props.cellsV = 50
+						items["5811ce572459770cba1a34ea"]._props.Grids[0]._props.cellsV = 100
+						items["5811ce662459770f6f490f32"]._props.Grids[0]._props.cellsV = 150
+						items["5811ce772459770e9e5f9532"]._props.Grids[0]._props.cellsV = 200
+					} catch (error) {
+						logger.warning("\nHideoutOptions.StashOptions.BiggerStash failed. Send bug report. Continue safely.")
+						log(error)
+					}
+				}
+
+				const originalStages = tables.hideout.areas.find((x) => x._id == "5d484fc0654e76006657e0ab").stages
+
+				for (const stage in originalStages) {
+					if (config.HideoutOptions.StashOptions.Easier_Loyalty.enabled == true) {
+						try {
+							originalStages[stage].requirements
+								.filter((x) => x.loyaltyLevel != undefined)
+								.forEach((x) => {
+									x.loyaltyLevel -= 1
+								})
+						} catch (error) {
+							logger.warning("\nHideoutOptions.StashOptions.Easier_Loyalty failed. Send bug report. Continue safely.")
+							log(error)
+						}
+					}
+
+					if (config.HideoutOptions.StashOptions.Less_Currency_For_Construction.enabled == true) {
+						try {
+							originalStages[stage].requirements
+								.filter((x) => x.templateId == "5449016a4bdc2d6f028b456f" || x.templateId == "569668774bdc2da2298b4568")
+								.forEach((x) => {
+									x.count /= 10
+								})
+						} catch (error) {
+							logger.warning("\nHideoutOptions.StashOptions.Less_Currency_For_Construction failed. Send bug report. Continue safely.")
+							log(error)
+						}
+					}
+				}
+
+				try {
+					tables.hideout.areas.find((x) => x._id == "5d484fc0654e76006657e0ab").stages = originalStages
+				} catch (error) {
+					logger.warning("\nHideoutOptions.StashOptions (Easier_Loyalty or Less_Currency_For_Construction) failed. Send bug report. Continue safely.")
+					log(error)
+				}
+
+				if (config.HideoutOptions.StashOptions.Progressive_Stash.enabled == true) {
+					const basicStashBonuses = [
+						{
+							id: "64f5b9e5fa34f11b380756c0",
+							templateId: "566abbc34bdc2d92178b4576",
+							type: "StashSize",
+						},
+					]
+					for (const profile of profileList) {
+						try {
+							tables.templates.profiles[profile].bear.character.Hideout.Areas.find((x) => x.type == "3").level = 1
+							tables.templates.profiles[profile].usec.character.Hideout.Areas.find((x) => x.type == "3").level = 1
+
+							tables.templates.profiles[profile].bear.character.Inventory.items
+								.filter((x) => x._tpl == "5811ce572459770cba1a34ea" || x._tpl == "5811ce662459770f6f490f32" || x._tpl == "5811ce772459770e9e5f9532")
+								.forEach((element) => {
+									element._tpl = "566abbc34bdc2d92178b4576"
+								})
+							tables.templates.profiles[profile].usec.character.Inventory.items
+								.filter((x) => x._tpl == "5811ce572459770cba1a34ea" || x._tpl == "5811ce662459770f6f490f32" || x._tpl == "5811ce772459770e9e5f9532")
+								.forEach((element) => {
+									element._tpl = "566abbc34bdc2d92178b4576"
+								})
+							tables.templates.profiles[profile].bear.character.Bonuses = basicStashBonuses
+							tables.templates.profiles[profile].usec.character.Bonuses = basicStashBonuses
+						} catch (error) {
+							logger.warning("\nconfig.HideoutOptions.BiggerStash.Progressive_Stash error")
+							log(error)
+						}
+					}
+				}
+			}
+			// 100x Faster hideout production, 10x superwater and moonshine production, bitcoins
+			for (const prod in tables.hideout.production) {
+				const endProduct = tables.hideout.production[prod].endProduct
+				const productionTime = tables.hideout.production[prod].productionTime
+				if (
+					(endProduct == "5d1b376e86f774252519444e" || endProduct == "5d1b33a686f7742523398398") &&
+					config.HideoutOptions.Faster_Moonshine_and_Purified_Water_Production.enabled
+				) {
+					// superwater and moonshine
+					tables.hideout.production[prod].productionTime = Math.round(
+						productionTime / config.HideoutOptions.Faster_Moonshine_and_Purified_Water_Production.Base_Moonshine_And_Water_Time_Multiplier
+					)
+				} else if (endProduct == "59faff1d86f7746c51718c9c" && config.HideoutOptions.Faster_Bitcoin_Farming.enabled == true) {
+					// bitcoins
+					tables.hideout.production[prod].productionTime = Math.round(
+						productionTime / config.HideoutOptions.Faster_Bitcoin_Farming.Base_Bitcoin_Time_Multiplier
+					)
+					if (config.HideoutOptions.Faster_Bitcoin_Farming.Revert_Bitcoin_Price_To_v012 == true) {
+						tables.templates.handbook.Items.find((x) => x.Id == "59faff1d86f7746c51718c9c").Price = 100000
+					}
+				} else if (config.HideoutOptions.Faster_Crafting_Time.enabled) {
+					// all other crafts
+					tables.hideout.production[prod].productionTime =
+						Math.round(productionTime / config.HideoutOptions.Faster_Crafting_Time.Base_Crafting_Time_Multiplier) + 1
+				}
+			}
+
+			if (config.HideoutOptions.Faster_Crafting_Time.enabled && config.HideoutOptions.Faster_Crafting_Time.Hideout_Skill_Exp_Fix.enabled) {
+				// Buff to hideout exp rate, more testing needed
+				hideoutConfig.hoursForSkillCrafting /= config.HideoutOptions.Faster_Crafting_Time.Hideout_Skill_Exp_Fix.Hideout_Skill_Exp_Multiplier
+			}
+
+			if (config.HideoutOptions.Faster_Bitcoin_Farming.enabled) {
+				// Instead of modifing base farming time try this:
+				tables.hideout.settings.gpuBoostRate = config.HideoutOptions.Faster_Bitcoin_Farming.GPU_Efficiency
+
+				// TODO: replace getBTCSlots() in HideoutHelper to add bonus slots per farm level. lv2 - 4 slots, lv3 - 5, elite - 7
+
+				// Vanilla: Base time (x1): 40h 17min (2417 min), GPU Boost (0.041225): x1, each GPU has only ~4.12% efficiency
+				// 2× GPU: 38h 41min x1.04
+				// 10× GPU: 29h 23min x1.37
+				// 25× GPU: 20h 15min x1.99
+				// 50× GPU: 13h 20min x3.02
+
+				// Softcore v0.1: Base time (x10): 4h 2min, GPU Boost (0.041225): x1
+				// 2× GPU: 3h 52min x1.04
+				// 10× GPU: 2h 56min x1.37
+				// 25× GPU: 2h 1min x1.99
+				// 50× GPU: 1h 20min x3.02
+
+				// Softcore v0.2: Base time (x2): 20h 8min, GPU Boost(0.5): x12.13, each GPU has ~50% efficiency
+				// 2× GPU: 13h 26min x1.5
+				// 10× GPU: 3h 40min x5.5
+				// 25× GPU: 1h 33min x13
+				// 50× GPU: 0h 47min x25.5
+
+				// Linear: Base time (x1): 40h 17min, GPU Boost (1): x24.26, each GPU is 100% efficient
+				// 2× GPU: 20h 8min x2
+				// 10× GPU: 4h 2min x10
+				// 25× GPU: 1h 37min x25
+				// 50× GPU: 0h 48min x50
+			}
+
+			if (config.HideoutOptions.Faster_Hideout_Construction.enabled) {
+				// 100x Faster hideout construction
+				for (const area in tables.hideout.areas) {
+					for (const stage in tables.hideout.areas[area].stages) {
+						tables.hideout.areas[area].stages[stage].constructionTime = Math.round(
+							tables.hideout.areas[area].stages[stage].constructionTime / config.HideoutOptions.Faster_Hideout_Construction.Hideout_Construction_Time_Multiplier
+						)
+					}
+				}
+			}
+
+			if (config.HideoutOptions.Increased_Fuel_Consumption.enabled) {
+				// 10x faster fuel draw
+				tables.hideout.settings.generatorFuelFlowRate *= config.HideoutOptions.Increased_Fuel_Consumption.Fuel_Consumption_Multiplier
+			}
+		}
+
 		if (config.AdditionalCraftingRecipes.enabled == true) {
 			try {
 				// 63da4dbee8fa73e225000001
@@ -1267,181 +1442,6 @@ class Mod implements IPostDBLoadMod {
 			} catch (error) {
 				logger.warning("\nAdditionalCraftingRecipes failed. Send bug report. Continue safely.")
 				log(error)
-			}
-		}
-
-		if (config.HideoutOptions.enabled) {
-			if (config.HideoutOptions.StashOptions.enabled) {
-				// Fix for ADHD.
-
-				// stash area id 5d484fc0654e76006657e0ab
-				// "_id": "566abbc34bdc2d92178b4576",
-				// "_name": "Standard stash 10x28",
-				// "_id": "5811ce572459770cba1a34ea",
-				// "_name": "Left Behind stash 10x38",
-				// "_id": "5811ce662459770f6f490f32",
-				// "_name": "Prepare for escape stash 10x48",
-				// "_id": "5811ce772459770e9e5f9532",
-				// "_name": "Edge of darkness stash 10x68",
-
-				if (config.HideoutOptions.StashOptions.BiggerStash.enabled) {
-					try {
-						items["566abbc34bdc2d92178b4576"]._props.Grids[0]._props.cellsV = 50
-						items["5811ce572459770cba1a34ea"]._props.Grids[0]._props.cellsV = 100
-						items["5811ce662459770f6f490f32"]._props.Grids[0]._props.cellsV = 150
-						items["5811ce772459770e9e5f9532"]._props.Grids[0]._props.cellsV = 200
-					} catch (error) {
-						logger.warning("\nHideoutOptions.StashOptions.BiggerStash failed. Send bug report. Continue safely.")
-						log(error)
-					}
-				}
-
-				const originalStages = tables.hideout.areas.find((x) => x._id == "5d484fc0654e76006657e0ab").stages
-
-				for (const stage in originalStages) {
-					if (config.HideoutOptions.StashOptions.Easier_Loyalty.enabled == true) {
-						try {
-							originalStages[stage].requirements
-								.filter((x) => x.loyaltyLevel != undefined)
-								.forEach((x) => {
-									x.loyaltyLevel -= 1
-								})
-						} catch (error) {
-							logger.warning("\nHideoutOptions.StashOptions.Easier_Loyalty failed. Send bug report. Continue safely.")
-							log(error)
-						}
-					}
-
-					if (config.HideoutOptions.StashOptions.Less_Currency_For_Construction.enabled == true) {
-						try {
-							originalStages[stage].requirements
-								.filter((x) => x.templateId == "5449016a4bdc2d6f028b456f" || x.templateId == "569668774bdc2da2298b4568")
-								.forEach((x) => {
-									x.count /= 10
-								})
-						} catch (error) {
-							logger.warning("\nHideoutOptions.StashOptions.Less_Currency_For_Construction failed. Send bug report. Continue safely.")
-							log(error)
-						}
-					}
-				}
-
-				try {
-					tables.hideout.areas.find((x) => x._id == "5d484fc0654e76006657e0ab").stages = originalStages
-				} catch (error) {
-					logger.warning("\nHideoutOptions.StashOptions (Easier_Loyalty or Less_Currency_For_Construction) failed. Send bug report. Continue safely.")
-					log(error)
-				}
-
-				if (config.HideoutOptions.StashOptions.Progressive_Stash.enabled == true) {
-					const basicStashBonuses = [
-						{
-							id: "64f5b9e5fa34f11b380756c0",
-							templateId: "566abbc34bdc2d92178b4576",
-							type: "StashSize",
-						},
-					]
-					for (const profile of profileList) {
-						try {
-							tables.templates.profiles[profile].bear.character.Hideout.Areas.find((x) => x.type == "3").level = 1
-							tables.templates.profiles[profile].usec.character.Hideout.Areas.find((x) => x.type == "3").level = 1
-
-							tables.templates.profiles[profile].bear.character.Inventory.items
-								.filter((x) => x._tpl == "5811ce572459770cba1a34ea" || x._tpl == "5811ce662459770f6f490f32" || x._tpl == "5811ce772459770e9e5f9532")
-								.forEach((element) => {
-									element._tpl = "566abbc34bdc2d92178b4576"
-								})
-							tables.templates.profiles[profile].usec.character.Inventory.items
-								.filter((x) => x._tpl == "5811ce572459770cba1a34ea" || x._tpl == "5811ce662459770f6f490f32" || x._tpl == "5811ce772459770e9e5f9532")
-								.forEach((element) => {
-									element._tpl = "566abbc34bdc2d92178b4576"
-								})
-							tables.templates.profiles[profile].bear.character.Bonuses = basicStashBonuses
-							tables.templates.profiles[profile].usec.character.Bonuses = basicStashBonuses
-						} catch (error) {
-							logger.warning("\nconfig.HideoutOptions.BiggerStash.Progressive_Stash error")
-							log(error)
-						}
-					}
-				}
-			}
-			// 100x Faster hideout production, 10x superwater and moonshine production, bitcoins
-			for (const prod in tables.hideout.production) {
-				const endProduct = tables.hideout.production[prod].endProduct
-				const productionTime = tables.hideout.production[prod].productionTime
-				if (
-					(endProduct == "5d1b376e86f774252519444e" || endProduct == "5d1b33a686f7742523398398") &&
-					config.HideoutOptions.Faster_Moonshine_and_Purified_Water_Production.enabled
-				) {
-					// superwater and moonshine
-					tables.hideout.production[prod].productionTime = Math.round(
-						productionTime / config.HideoutOptions.Faster_Moonshine_and_Purified_Water_Production.Base_Moonshine_And_Water_Time_Multiplier
-					)
-				} else if (endProduct == "59faff1d86f7746c51718c9c" && config.HideoutOptions.Faster_Bitcoin_Farming.enabled == true) {
-					// bitcoins
-					tables.hideout.production[prod].productionTime = Math.round(
-						productionTime / config.HideoutOptions.Faster_Bitcoin_Farming.Base_Bitcoin_Time_Multiplier
-					)
-					if (config.HideoutOptions.Faster_Bitcoin_Farming.Revert_Bitcoin_Price_To_v012 == true) {
-						tables.templates.handbook.Items.find((x) => x.Id == "59faff1d86f7746c51718c9c").Price = 100000
-					}
-				} else if (config.HideoutOptions.Faster_Crafting_Time.enabled) {
-					// all other crafts
-					tables.hideout.production[prod].productionTime =
-						Math.round(productionTime / config.HideoutOptions.Faster_Crafting_Time.Base_Crafting_Time_Multiplier) + 1
-				}
-			}
-
-			if (config.HideoutOptions.Faster_Crafting_Time.enabled && config.HideoutOptions.Faster_Crafting_Time.Hideout_Skill_Exp_Fix.enabled) {
-				// Buff to hideout exp rate, more testing needed
-				hideoutConfig.hoursForSkillCrafting /= config.HideoutOptions.Faster_Crafting_Time.Hideout_Skill_Exp_Fix.Hideout_Skill_Exp_Multiplier
-			}
-
-			if (config.HideoutOptions.Faster_Bitcoin_Farming.enabled) {
-				// Instead of modifing base farming time try this:
-				tables.hideout.settings.gpuBoostRate = config.HideoutOptions.Faster_Bitcoin_Farming.GPU_Efficiency
-
-				// TODO: replace getBTCSlots() in HideoutHelper to add bonus slots per farm level. lv2 - 4 slots, lv3 - 5, elite - 7
-
-				// Vanilla: Base time (x1): 40h 17min (2417 min), GPU Boost (0.041225): x1, each GPU has only ~4.12% efficiency
-				// 2× GPU: 38h 41min x1.04
-				// 10× GPU: 29h 23min x1.37
-				// 25× GPU: 20h 15min x1.99
-				// 50× GPU: 13h 20min x3.02
-
-				// Softcore v0.1: Base time (x10): 4h 2min, GPU Boost (0.041225): x1
-				// 2× GPU: 3h 52min x1.04
-				// 10× GPU: 2h 56min x1.37
-				// 25× GPU: 2h 1min x1.99
-				// 50× GPU: 1h 20min x3.02
-
-				// Softcore v0.2: Base time (x2): 20h 8min, GPU Boost(0.5): x12.13, each GPU has ~50% efficiency
-				// 2× GPU: 13h 26min x1.5
-				// 10× GPU: 3h 40min x5.5
-				// 25× GPU: 1h 33min x13
-				// 50× GPU: 0h 47min x25.5
-
-				// Linear: Base time (x1): 40h 17min, GPU Boost (1): x24.26, each GPU is 100% efficient
-				// 2× GPU: 20h 8min x2
-				// 10× GPU: 4h 2min x10
-				// 25× GPU: 1h 37min x25
-				// 50× GPU: 0h 48min x50
-			}
-
-			if (config.HideoutOptions.Faster_Hideout_Construction.enabled) {
-				// 100x Faster hideout construction
-				for (const area in tables.hideout.areas) {
-					for (const stage in tables.hideout.areas[area].stages) {
-						tables.hideout.areas[area].stages[stage].constructionTime = Math.round(
-							tables.hideout.areas[area].stages[stage].constructionTime / config.HideoutOptions.Faster_Hideout_Construction.Hideout_Construction_Time_Multiplier
-						)
-					}
-				}
-			}
-
-			if (config.HideoutOptions.Increased_Fuel_Consumption.enabled) {
-				// 10x faster fuel draw
-				tables.hideout.settings.generatorFuelFlowRate *= config.HideoutOptions.Increased_Fuel_Consumption.Fuel_Consumption_Multiplier
 			}
 		}
 
