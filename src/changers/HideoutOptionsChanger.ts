2b08a866dde6c1de8ac6f7b60922cad0b85ccdfc
TetrisGG <tetrisggdev@gmail.com>
2024-12-17 00:14:55 +0100

    Implemented bigger hideout containers

--- src/changers/HideoutOptionsChanger.ts
+++ src/changers/HideoutOptionsChanger.ts
@@ -26,8 +26,36 @@ export class HideoutOptionsChanger {
         if (!config.enabled) {
             return;
         }
-        if (config.stashOptions.enabled){
+        if (config.stashOptions.enabled) {
             new StashOptionsChanger(this.container).apply(config.stashOptions);
+        }
+        if (config.hideoutContainers.enabled) {
+            this.doHideoutContainers();
+        }
+    }
+
+    private doHideoutContainers() {
+        if (!this.items) {
+            this.logger.warning("HideoutOptions: doHideoutContainers: items table not found");
+            return;
+        }
+
+        const containersToModify = [
+            { tpl: ItemTpl.CONTAINER_MEDICINE_CASE, cellsH: 10, cellsV: 10 },
+            { tpl: ItemTpl.CONTAINER_MR_HOLODILNICK_THERMAL_BAG, cellsH: 10, cellsV: 10 },
+            { tpl: ItemTpl.CONTAINER_MAGAZINE_CASE, cellsH: 10, cellsV: 7 },
+            { tpl: ItemTpl.CONTAINER_ITEM_CASE, cellsH: 10, cellsV: 10 },
+            { tpl: ItemTpl.CONTAINER_WEAPON_CASE, cellsH: 6, cellsV: 10 },
+        ];
+
+        for (const container of containersToModify) {
+            const item = this.items[container.tpl];
+            if (item?._props?.Grids?.[0]?._props) {
+                item._props.Grids[0]._props.cellsH = container.cellsH;
+                item._props.Grids[0]._props.cellsV = container.cellsV;
+            } else {
+                this.logger.warning(`HideoutContainers: ${container.tpl} not found.`);
+            }
         }
     }
 }
