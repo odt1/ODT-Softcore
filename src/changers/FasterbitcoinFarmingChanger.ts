3d4fff3e045fe87bc9dec2bc47b35fd76c8f439e
TetrisGG <tetrisggdev@gmail.com>
2024-12-17 11:43:51 +0100

    Optimized Imports

--- src/changers/FasterbitcoinFarmingChanger.ts
+++ src/changers/FasterbitcoinFarmingChanger.ts
@@ -2,22 +2,18 @@ import { DependencyContainer } from "tsyringe";
 import { DatabaseServer } from "@spt/servers/DatabaseServer";
 import { FasterBitcoinFarming } from "src/types";
 import { IDatabaseTables } from "@spt/models/spt/server/IDatabaseTables";
-import { ITemplateItem } from "@spt/models/eft/common/tables/ITemplateItem";
 import { ItemTpl } from "@spt/models/enums/ItemTpl";
 import { PrefixLogger } from "src/util/PrefixLogger";
-import { IHideoutProduction } from "@spt/models/eft/hideout/IHideoutProduction";
 
 export class FasterBitcoinFarmingChanger {
     private logger: PrefixLogger;
     private databaseServer: DatabaseServer;
     private tables: IDatabaseTables;
-    private items: Record<string, ITemplateItem> | undefined;
 
     constructor(container: DependencyContainer) {
         this.logger = PrefixLogger.getInstance();
         this.databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
         this.tables = this.databaseServer.getTables();
-        this.items = this.tables.templates?.items;
     }
 
     public apply(config: FasterBitcoinFarming) {
